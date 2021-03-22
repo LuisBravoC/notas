80a53f77cb97def37b6bed351aa49aba1522b913
Luis Manuel Bravo Cazares <luismanuelbravocazares@gmail.com>
2021-03-22 11:48:49 -0700

    make it better

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,7 +1,50 @@
 const router = require('express').Router();
+const Note = require('../models/Notes');
 
-router.get('/notes', (req, res) =>{
-    res.send('Notas');
+router.get('/notes/add', (req, res) =>{
+    res.render('notes/new-notes');
 });
+
+router.post('/notes/new-notes', async (req, res) =>{
+    console.log(req.body);
+    const { title, description}= req.body;
+    const errors= [];
+    if (!title){
+        errors.push({text: 'Por favor escriba un titulo'});
+    }
+    if (!description){
+        errors.push({text: 'Por favor escriba una descripción'});
+    }
+
+    if(errors.length > 0){
+        res.render('notes/new-notes', {
+            errors,
+            title,
+            description
+        });
+    }else{
+        const newNote = new Note({title,description});
+        await newNote.save();
+        console.log(newNote);
+        res.redirect('/notes');
+    }
+});
+
+router.get('/notes', async (req, res) => {
+    await Note.find()
+        .then(documentos => {
+            const contexto = {
+                notes: documentos.map(documento => {
+                    return {
+                        title: documento.title,
+                        description: documento.description
+                    }
+                })
+            }
+            res.render('notes/all-notes', {
+                notes: contexto.notes
+            })
+        })
+})
 
 module.exports = router;
